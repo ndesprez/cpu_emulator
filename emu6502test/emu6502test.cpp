6d3f0913bb898173fc025799de60782bccd1c7dc
ndesprez <nicolas.desprez@gmail.com>
2021-05-14 14:53:30 +0200

    all load tests implemented, some store tests done

--- emu6502test/emu6502test.cpp
+++ emu6502test/emu6502test.cpp
@@ -6,29 +6,37 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace emu6502test
 {
-	TEST_CLASS(Load)
+	Processor *MOS6502;
+	byte	  *Memory;
+
+	static void _method_initialize()
+	{
+		Memory = new byte[0x10000];
+		Memory[0xFFFC] = 0x00;
+		Memory[0xFFFD] = 0x10;
+
+		MOS6502 = new Processor(Memory);
+		MOS6502->SendRST();
+		MOS6502->Step();
+	}
+
+	static void _method_cleanup()
+	{
+		delete MOS6502;
+		delete[] Memory;
+	}
+
+	TEST_CLASS(LoadInstructions)
 	{
 	public:
-		Processor *MOS6502;
-		byte	  *Memory;
-
-
 		TEST_METHOD_INITIALIZE(createCPU)
 		{
-			// method initialization code
-			Memory = new byte[0x10000];
-			Memory[0xFFFC] = 0x00;
-			Memory[0xFFFD] = 0x10;
-
-			MOS6502 = new Processor(Memory);
-			MOS6502->SendRST();
-			MOS6502->Step();
+			_method_initialize();
 		}
 
 		TEST_METHOD_CLEANUP(deleteCPU)
 		{
-			delete MOS6502;
-			delete[] Memory;
+			_method_cleanup();
 		}
 
 		TEST_METHOD(LDA_IMM)
@@ -110,6 +118,20 @@ namespace emu6502test
 			Assert::AreEqual((int)MOS6502->A, 0xDF);
 		}
 
+		TEST_METHOD(LDA_INDY)
+		{
+			Memory[0x1000] = 0xA0;
+			Memory[0x1001] = 0x30;
+			Memory[0x1002] = 0xB1;
+			Memory[0x1003] = 0x20;
+			Memory[0x0020] = 0x00;
+			Memory[0x0021] = 0x40;
+			Memory[0x4030] = 0xE0;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->A, 0xE0);
+		}
+
 		TEST_METHOD(LDX_IMM)
 		{
 			Memory[0x1000] = 0xA2;
@@ -128,6 +150,40 @@ namespace emu6502test
 			Assert::AreEqual((int)MOS6502->X, 0xC8);
 		}
 
+		TEST_METHOD(LDX_ABSY)
+		{
+			Memory[0x1000] = 0xA0;
+			Memory[0x1001] = 0x30;
+			Memory[0x1002] = 0xBE;
+			Memory[0x1003] = 0x00;
+			Memory[0x1004] = 0x20;
+			Memory[0x2030] = 0xC9;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->X, 0xC9);
+		}
+
+		TEST_METHOD(LDX_ZPG)
+		{
+			Memory[0x1000] = 0xA6;
+			Memory[0x1001] = 0x40;
+			Memory[0x0040] = 0xDE;
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->X, 0xDE);
+		}
+
+		TEST_METHOD(LDX_ZPGY)
+		{
+			Memory[0x1000] = 0xA0;
+			Memory[0x1001] = 0x91;
+			Memory[0x1002] = 0xB6;
+			Memory[0x1003] = 0x80;
+			Memory[0x0011] = 0xDF;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->X, 0xDF);
+		}
+
 		TEST_METHOD(LDY_IMM)
 		{
 			Memory[0x1000] = 0xA0;
@@ -144,6 +200,136 @@ namespace emu6502test
 			Memory[0x2000] = 0x92;
 			MOS6502->Step();
 			Assert::AreEqual((int)MOS6502->Y, 0x92);
+		}
+		
+		TEST_METHOD(LDY_ABSX)
+		{
+			Memory[0x1000] = 0xA2;
+			Memory[0x1001] = 0x30;
+			Memory[0x1002] = 0xBC;
+			Memory[0x1003] = 0x00;
+			Memory[0x1004] = 0x20;
+			Memory[0x2030] = 0xCA;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->Y, 0xCA);
+		}
+
+		TEST_METHOD(LDY_ZPG)
+		{
+			Memory[0x1000] = 0xA4;
+			Memory[0x1001] = 0x50;
+			Memory[0x0050] = 0xDF;
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->Y, 0xDF);
+		}
+
+		TEST_METHOD(LDY_ZPGX)
+		{
+			Memory[0x1000] = 0xA2;
+			Memory[0x1001] = 0x20;
+			Memory[0x1002] = 0xB4;
+			Memory[0x1003] = 0x80;
+			Memory[0x00A0] = 0xE0;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)MOS6502->Y, 0xE0);
+		}	
+	};
+
+	TEST_CLASS(StoreInstructions)
+	{
+		TEST_METHOD_INITIALIZE(createCPU)
+		{
+			_method_initialize();
+		}
+
+		TEST_METHOD_CLEANUP(deleteCPU)
+		{
+			_method_cleanup();
+		}
+
+		TEST_METHOD(STA_ABS)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xD5;
+			Memory[0x1002] = 0x8D;
+			Memory[0x1003] = 0x00;
+			Memory[0x1004] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x2000], 0xD5);
+		}
+
+		TEST_METHOD(STA_ABSX)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xD6;
+			Memory[0x1002] = 0xA2;
+			Memory[0x1003] = 0xC7;
+			Memory[0x1004] = 0x9D;
+			Memory[0x1005] = 0x00;
+			Memory[0x1006] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x20C7], 0xD6);
+		}
+
+		TEST_METHOD(STA_ABSY)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xD7;
+			Memory[0x1002] = 0xA0;
+			Memory[0x1003] = 0xB7;
+			Memory[0x1004] = 0x99;
+			Memory[0x1005] = 0x00;
+			Memory[0x1006] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x20B7], 0xD7);
+		}
+
+		TEST_METHOD(STA_ZPG)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xD8;
+			Memory[0x1002] = 0x85;
+			Memory[0x1003] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x0020], 0xD8);
+		}
+
+		TEST_METHOD(STA_ZPGX)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xD9;
+			Memory[0x1002] = 0xA2;
+			Memory[0x1003] = 0x10;
+			Memory[0x1004] = 0x95;
+			Memory[0x1005] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x0030], 0xD9);
+		}
+
+		TEST_METHOD(STA_XIND)
+		{
+			Memory[0x1000] = 0xA9;
+			Memory[0x1001] = 0xDA;
+			Memory[0x1002] = 0xA2;
+			Memory[0x1003] = 0x10;
+			Memory[0x1004] = 0x81;
+			Memory[0x1005] = 0x20;
+			Memory[0x0030] = 0x16;
+			Memory[0x0031] = 0x20;
+			MOS6502->Step();
+			MOS6502->Step();
+			MOS6502->Step();
+			Assert::AreEqual((int)Memory[0x2016], 0xDA);
 		}
 	};
 }
